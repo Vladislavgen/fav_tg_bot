5b91df99afc2997f4b077c1bd0eee7464bb7b6d6
MacBook <macbook@MacBook-Air-MacBook.local>
2020-05-27 20:16:28 +0300

    fixbot

--- bot.py
+++ bot.py
@@ -14,15 +14,6 @@ bot = telebot.TeleBot(token)
 #     print(message.text)
 
 
-
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
-
 @bot.message_handler(commands=['help'])
 def help(message):
     text = '''
