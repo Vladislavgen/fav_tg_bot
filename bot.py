89773c245f60c0aa8d866e02686c4955635c3950
MacBook <macbook@MacBook-Air-MacBook.local>
2020-05-27 19:37:59 +0300

    fixbot

--- bot.py
+++ bot.py
@@ -4,9 +4,23 @@ import telebot
 token = '1221249595:AAENfywP9LTew9ciIcdM0ajHDtarBJNuG-k'
 bot = telebot.TeleBot(token)
 
+
+
+
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     bot.send_message(message.chat.id, message.text)
+    print(message)
+    print(message.chat.id)
+    print(message.text)
+
+
+
+@bot.message_handler(content_types=["text"])
+def answer_to_hi(message):
+    bot.send_message(message.chat.id, message.text)
+
 
 if __name__ == '__main__':
      bot.polling(none_stop=True)
+
