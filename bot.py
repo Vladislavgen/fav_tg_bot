8c54363c957611193acbe92fc833bd1280d0b779
MacBook <macbook@MacBook-Air-MacBook.local>
2020-05-27 19:45:55 +0300

    fixbot

--- bot.py
+++ bot.py
@@ -7,12 +7,12 @@ bot = telebot.TeleBot(token)
 
 
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
-    print(message)
-    print(message.chat.id)
-    print(message.text)
+# @bot.message_handler(content_types=["text"])
+# def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+#     bot.send_message(message.chat.id, message.text)
+#     print(message)
+#     print(message.chat.id)
+#     print(message.text)
 
 
 
