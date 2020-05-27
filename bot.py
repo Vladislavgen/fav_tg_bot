52c5f0400a494b4f3eaf3758d829ef5404e81599
MacBook <macbook@MacBook-Air-MacBook.local>
2020-05-27 19:16:31 +0300

    fix token

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 
 import telebot
 token = '1221249595:AAENfywP9LTew9ciIcdM0ajHDtarBJNuG-k'
-bot = telebot.TeleBot(config.token)
+bot = telebot.TeleBot(token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
